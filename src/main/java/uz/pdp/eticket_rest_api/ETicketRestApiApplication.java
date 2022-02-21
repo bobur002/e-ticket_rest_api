5719bbb33ab55db9bd3efd68de84696ac9261cf8
user <boburerkinov002@gmail.com>
2022-02-21 19:13:14 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/eticket_rest_api/ETicketRestApiApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.eticket_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicketRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ETicketRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/eticket_rest_api/ETicketRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.eticket_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicketRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
